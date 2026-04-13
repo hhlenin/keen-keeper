2ef0df2077f4378fbbc64185c50dfa59363833b5
Hasibul Lenin <hasibul.lenin@gmail.com>
2026-04-13 12:55:11 -0400

    defining routes, layout setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <h1>Hello World!</h1>,
+            },
+            {
+                path: '/details/:id',
+                element: <h1>Details!</h1>,
+            },
+            {
+                path: '/timeline',
+                element: <h1>Timeline!</h1>,
+            },
+            {
+                path: '/stats',
+                element: <h1>Stats!</h1>,
+            },
+            {
+                path: '*',
+                element: <h1>Error!</h1>,
+            }
+        ]
+    }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
